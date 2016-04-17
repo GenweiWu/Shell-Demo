ec704bc53b0705efc4444c840064c4191abe0037
GenweiWu <wugenwei2010@163.com>
2016-04-17 18:37:31 +0800

    shell  变量扩展

---
+++ variable/expansion/test01.sh
@@ -0,0 +1,63 @@
+function test1()
+{
+	line=1aaa2
+	echo "${line}"
+	echo "${line/a/b}"
+	echo "${line//a/b}"
+	echo "${line//a/}"
+}
+
+function test1a(){
+	line=123a456a789
+    echo "$line"
+	#说明/是最长匹配的
+    echo "${line/*a/}"
+    echo "${line//*a/}"
+    echo "${line/a*/}"
+    echo "${line//a*/}"
+}
+
+function test1b(){
+	line=0ab0
+	echo "${line}"
+	echo "${line/a*/-}"
+	echo "${line/#a*/-}"
+
+	echo "${line/*b/-}"
+	echo "${line/%*b/-}"
+}
+
+function test2()
+{
+	line=123a456a789
+	echo "${line}"
+	echo "${line#*a}"
+	echo "${line##*a}"
+
+	#echo "${line#a*}"
+	#echo "${line##a*}"
+}
+
+function test3()
+{
+	line=123a456a789
+	echo "${line}"
+	echo "${line#*a}"
+	echo "${line##*a}"
+
+	#echo "${line#a*}"
+	#echo "${line##a*}"
+
+}
+
+test1
+
+echo -----------------;test1a
+
+echo -----------------;test1b
+
+echo -----------------;test2
+
+echo -----------------;test3
+
+
